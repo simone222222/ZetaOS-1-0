ebc148c78f6df2dea236461fd5c437e7b01ff35b
simone222222 <simone.paolini14@gmail.com>
2026-01-03 21:25:41 +0100

    Add compilation of gdt.c in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 nasm -f elf32 boot.asm -o boot.o
 gcc -m32 -c kernel.c -o kernel.o
 gcc -m32 -c ./src/print.c -o ./src/print.o
+gcc -m32 -c ./src/gdt.c -o ./src/gdt.o
 ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
 qemu-system-x86_64 -kernel ./bin/kernel.bin
