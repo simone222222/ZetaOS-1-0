3ddee2375d590df83fd7ca3acf025274e735da1b
simone222222 <simone.paolini14@gmail.com>
2026-01-03 21:24:27 +0100

    Include GDT initialization in kernel_main

--- kernel.c
+++ kernel.c
@@ -1,8 +1,11 @@
 #include "./include/print.h"
+#include "./include/gdt.h"
 
 void kernel_main() {
     // Simple usage of print_msg() to print a string to the screen
     clear_screen();
     print_msg("kernel_main() has been called!\n");
     print_msg("print_msg() has been called!\n");
+    init_gdt();
+    print_msg("init_gdt() has benn called!\n");
 }
